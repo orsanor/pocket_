ef95be07690075b0e37bef94b5159a525d58110d
orsano <orsanor@gmail.com>
2024-09-19 03:45:51 -0300

    finalizando aula 2 do NLW

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,23 +1,17 @@
 // JSX - JAVASCRIPT XML -> HMTL
-import { Plus } from 'lucide-react'
-import logo from './assets/logo-in-orbit.svg'
-import letsStart from './assets/lets-start-illustration.svg'
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/ui/create-goal'
+import { EmptyGoals } from './components/ui/empty-goals'
+import { Summary } from './components/ui/summary'
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="in.orbit" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
-        <Plus className="size-4" />
-        Cadastrar meta
-      </button>
-    </div>
+    <Dialog>
+      {/* { <EmptyGoals/>*/}
+
+      <Summary />
+
+      <CreateGoal />
+    </Dialog>
   )
 }
